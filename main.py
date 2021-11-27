2142accd7013e804c032661f8bd7c68744e97795
robertocannella <robertocannella@gmail.com>
2021-11-27 14:46:41 -0500

    Refactored for cleanliness

--- main.py
+++ main.py
@@ -4,10 +4,12 @@ import matplotlib.pyplot as plt
 import matplotlib as mpl
 from scipy import stats
 from matplotlib.backend_bases import MouseButton
+import plotly.express as px
+
 import math
 
+# Generate random data
 np.random.seed(12345)
-
 df = pd.DataFrame([np.random.normal(32000,200000,3650),
                    np.random.normal(43000,100000,3650),
                    np.random.normal(43500,140000,3650),
@@ -15,25 +17,18 @@ df = pd.DataFrame([np.random.normal(32000,200000,3650),
                   index=[1992,1993,1994,1995]).T
 
 
-fig = plt.figure(figsize=(5,5))
-y_value = df.mean().values.max()
+# Set figure defaults here.
+
+fig = plt.figure(figsize=(5, 5))
+
+# Set parameter defaults here.
+y_value = df.mean().values.mean()                    # initial y value
+y_error = stats.sem(df)*1.96                         # Standard Error * Confidence Level
+cmap = mpl.cm.RdBu_r                                 # Color scheme
 
 
 
-y_error = df.std().values/ (math.sqrt(df.count().values[0]))
-print (y_error)
-y_error = stats.sem(df)*1.96
-print (y_error)
-
-y_value = 30000
-
-cmap = mpl.cm.RdBu_r
-
-
-x_coordinates = [df.columns.min(),df.columns.max()]
-
-
-cmap = mpl.cm.RdBu_r
+# Plot bars.
 for n, item in enumerate(df.columns):
     norm = mpl.colors.Normalize(df[item].mean() - y_error[n], df[item].mean() + y_error[n])
     colors = cmap(norm(abs(y_value)))
@@ -43,18 +38,21 @@ for n, item in enumerate(df.columns):
                 edgecolor='grey', label='',
                 yerr=stats.sem(df[item]) * 1.96, capsize=10)
 _ = plt.xticks(df.columns)
+_ = plt.title('Mean Probability Comparison')
+line1 = plt.axhline(y=y_value, picker=5, color='k')
 
-cbar = fig.colorbar(mpl.cm.ScalarMappable(norm=norm, cmap=cmap),aspect=10)
-cbar.set_ticks([])
-print(df.mean().values)
-print (df.mean().values/42000)
+# Color bar configuration
+below = df.mean().values.min()-y_error.max()
+within = df.mean().values.mean()-y_error.min()
+above = df.mean().values.max()+ y_error.min()
+mean_normalize = mpl.colors.Normalize(below,above)
+cbar = fig.colorbar(mpl.cm.ScalarMappable(norm=mean_normalize, cmap=cmap), aspect=10)
+cbar.set_ticks([below, within, above])
+cbar.ax.set_yticklabels(['Below', 'Within', 'Above'])  # horizontal colorbar
 
-line1 = plt.axhline(y=y_value, picker=5)
-mouse_off = False
 
 def on_move(event):
     global line1
-    x, y = event.x, event.y
     if event.inaxes:
         _ = event.inaxes  # the axes instance
         y_value = event.ydata
@@ -69,17 +67,18 @@ def on_move(event):
                          edgecolor='grey', label='',
                          yerr=stats.sem(df[item])*1.96, capsize=10)
         _ = plt.xticks(df.columns)
+        _ = plt.title('Mean Probability Comparison')
 
-        mean_normalize = mpl.colors.Normalize(df.mean().values.min(),df.mean().values.max())
+
+        mean_normalize = mpl.colors.Normalize(below,above)
         cbar = fig.colorbar(mpl.cm.ScalarMappable(norm=mean_normalize, cmap=cmap),aspect=10)
-        cbar.set_ticks([df.mean().values.min(),df.mean().values.mean(),df.mean().values.max()])
+        cbar.set_ticks([below,within,above])
         cbar.ax.set_yticklabels(['Below', 'Within', 'Above'])  # horizontal colorbar
         #y_coordinates = [y_value, y_value]
-        line1 = plt.axhline(y=y_value, picker=5)
+        line1 = plt.axhline(y=y_value, picker=5, color='k')
         #plt.plot(x_coordinates, y_coordinates)
         plt.draw()
 
-binding_id = ''
 
 def on_click(event):
     print(event.inaxes)
